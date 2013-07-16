b467036bbb190c0ebb2ee1c2af70ee98abdb39b4
godgunman <godgunman@gmail.com>
2013-07-16 15:16:46 +0800

    refactor SimpleUI3, make file name a constant value

--- examples/SimpleUI3/src/com/example/simpleui3/MainActivity.java
+++ examples/SimpleUI3/src/com/example/simpleui3/MainActivity.java
@@ -27,94 +27,93 @@ public class MainActivity extends Activity {
 
 	private SharedPreferences sp;
 	private SharedPreferences.Editor edit;
-	
+
 	/*
-	 * data/data/com.example.simpleui3/shared_prefs/message.xml
+	 * data/data/com.example.simpleui3/shared_prefs/settings.xml
 	 */
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.simple_ui);
-        
-        sp = getSharedPreferences("message", Context.MODE_PRIVATE);
-        edit = sp.edit();
-                
-        submitButton = (Button) findViewById(R.id.submitButton);
-        inputEdit = (EditText) findViewById(R.id.input);
-        isEncrypt = (CheckBox) findViewById(R.id.encrypt);
-        
-        boolean isChecked = sp.getBoolean("check_box", true);
-        String text = sp.getString("edit_text", "");
-        
-        inputEdit.setText(text);
-        isEncrypt.setChecked(isChecked);
-        
-        submitButton.setOnClickListener(new OnClickListener() {
-			
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.simple_ui);
+
+		sp = getSharedPreferences("settings", Context.MODE_PRIVATE);
+		edit = sp.edit();
+
+		submitButton = (Button) findViewById(R.id.submitButton);
+		inputEdit = (EditText) findViewById(R.id.input);
+		isEncrypt = (CheckBox) findViewById(R.id.encrypt);
+
+		boolean isChecked = sp.getBoolean("check_box", true);
+		String text = sp.getString("edit_text", "");
+
+		inputEdit.setText(text);
+		isEncrypt.setChecked(isChecked);
+
+		submitButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				Log.d("debug", "click");
-				
-				Editable editable = inputEdit.getText();
-				String text = editable.toString();
-				if (isEncrypt.isChecked()) {
-					text = "************";
-				}
-				
-				editable.clear();
-				Toast.makeText(MainActivity.this, 
-						text, Toast.LENGTH_SHORT).show();
-				
-				Intent intent = new Intent();
-				intent.putExtra("message", text);
-				intent.setClass(MainActivity.this, Message.class);
-				
-				MainActivity.this.startActivity(intent);
+				sendMessage();
 			}
 		});
-        
-        
-        isEncrypt.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
+
+		isEncrypt.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
 			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		        edit.putBoolean("check_box", isChecked);
-		        edit.commit();
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				edit.putBoolean("check_box", isChecked);
+				edit.commit();
 			}
 		});
-        
-        inputEdit.setOnKeyListener(new OnKeyListener() {
-			
+
+		inputEdit.setOnKeyListener(new OnKeyListener() {
+
 			@Override
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
 				Editable editable = inputEdit.getText();
 				String text = editable.toString();
 
-				if(event.getAction() == KeyEvent.ACTION_DOWN){
-					if(keyCode == KeyEvent.KEYCODE_ENTER) {
-						
-						editable.clear();
-						Toast.makeText(MainActivity.this, 
-								text, Toast.LENGTH_SHORT).show();
+				if (event.getAction() == KeyEvent.ACTION_DOWN) {
+					if (keyCode == KeyEvent.KEYCODE_ENTER) {
+						Log.d("debug", "enter");
+						sendMessage();
 						return true;
 					}
 				}
 
 				text = editable.toString();
 				edit.putString("edit_text", text);
-		        edit.commit();
+				edit.commit();
 				return false;
 			}
 		});
 
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	private void sendMessage() {
+		Editable editable = inputEdit.getText();
+		String text = editable.toString();
+		if (isEncrypt.isChecked()) {
+			text = "************";
+		}
+
+		editable.clear();
+		Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+
+		Intent intent = new Intent();
+		intent.putExtra("message", text);
+		intent.setClass(this, MessageActivity.class);
+
+		startActivity(intent);
+	}
 }
--- examples/SimpleUI3/src/com/example/simpleui3/MessageActivity.java
+++ examples/SimpleUI3/src/com/example/simpleui3/MessageActivity.java
@@ -13,36 +13,38 @@ import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Message extends Activity {
-	
-	ListView listView;
-	
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-    	setContentView(R.layout.message);
+public class MessageActivity extends Activity {
 
-    	listView = (ListView) findViewById(R.id.listView1);
-    	
-    	Intent intent = this.getIntent();
-    	String text = intent.getStringExtra("message");
-    	writeFile(text);
-    	
+	private ListView listView;
+	private static final String FILE_NAME = "MESSAGE_HISTORY.txt";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.message);
+
+		listView = (ListView) findViewById(R.id.listView1);
+
+		Intent intent = this.getIntent();
+		String text = intent.getStringExtra("message");
+		writeFile(text);
+
 		String[] content = getMessages();
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, content);
-		
+
 		listView.setAdapter(adapter);
-    }
+	}
 
 	private String[] getMessages() {
 		String allText = readFile();
 		return allText.split("\n");
 	}
-	
+
 	private void writeFile(String text) {
-    	try {
-			FileOutputStream fos = openFileOutput("message.txt", Context.MODE_APPEND);
+		try {
+			FileOutputStream fos = openFileOutput(FILE_NAME,
+					Context.MODE_APPEND);
 			text = text + "\n";
 			fos.write(text.getBytes());
 		} catch (FileNotFoundException e) {
@@ -53,26 +55,28 @@ public class Message extends Activity {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String readFile() {
 		String text = "";
-    	try {
-			FileInputStream fis = openFileInput("message.txt");
-			byte[] buffer = new byte[1024];			
+		try {
+			FileInputStream fis = openFileInput(FILE_NAME);
+
+			//read once, just for example.
+			byte[] buffer = new byte[1024];
 			fis.read(buffer);
 			text = new String(buffer);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}    	
-    	return text;
+		}
+		return text;
 	}
-	
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
